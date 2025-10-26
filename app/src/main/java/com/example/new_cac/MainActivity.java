66d455d6b99757eb3e2930bce095e587157083fa
TonyDH-dev <doyul.han@outlook.com>
2025-10-26 15:38:10 -0700

    copied code from seonmok for mainact and manifest

--- app/src/main/java/com/example/new_cac/MainActivity.java
+++ app/src/main/java/com/example/new_cac/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.new_cac;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        setActivityLayout(R.layout.activity_main);
+
+
+        Button teachingLibrary = findViewById(R.id.teachingLibrary);
+        Button timer = findViewById(R.id.timer);
+
+        teachingLibrary.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, TeachLIB.class));});
+
+        timer.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, Timer.class));});
+
     }
 }
